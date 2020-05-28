1281216ef4fa3e82775d27c84cac2c2cfdfdb5ed
AKrendels <38916450+AKrendels@users.noreply.github.com>
2020-05-28 17:16:52 +0200

    Added files
    Header is the provided one, just included the stdint.h header (for some reason needed on my system).
    child.c contains the implementation of the child function, programmed by me

---
+++ child.c
@@ -0,0 +1,29 @@
+#include "tree_based_quadrants.h"
+
+void child (const quadrant_t * q, quadrant_t * child_quadrant,
+                           int child_id)
+{
+    child_quadrant->level = q->level + 1;
+
+    child_quadrant->x = q->x + (child_id%2) * QUADRANT_LEN(child_quadrant->level);
+
+    if(child_id==2 || child_id==3 || child_id==6 || child_id==7 )
+    {
+        child_quadrant->y = q->y + QUADRANT_LEN(child_quadrant->level);
+    }
+    else
+    {
+        child_quadrant->y = q->y;
+    }
+
+    if(child_id>3)
+    {
+        child_quadrant->z = q->z + QUADRANT_LEN(child_quadrant->level);
+    }
+    else
+    {
+        child_quadrant->z = q->z;
+    }
+
+}
+
